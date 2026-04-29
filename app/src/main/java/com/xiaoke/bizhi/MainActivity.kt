eaaccd44ee6cc0f577ff26eace1c6aed94e3588d
ktkiss <114936588+ktkiss@users.noreply.github.com>
2026-04-29 10:24:41 +0800

    Fix MainActivity back button handling

--- app/src/main/java/com/xiaoke/bizhi/MainActivity.kt
+++ app/src/main/java/com/xiaoke/bizhi/MainActivity.kt
@@ -4,8 +4,8 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import android.webkit.WebSettings
 import android.widget.FrameLayout
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -13,50 +13,54 @@ class MainActivity : AppCompatActivity() {
     private var webView: WebView? = null
     private val url = "https://byd.xiaoke.name/"
 
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         
-        try {
-            // 简单创建 WebView
-            webView = WebView(this)
-            
-            setContentView(webView, FrameLayout.LayoutParams(
-                FrameLayout.LayoutParams.MATCH_PARENT,
-                FrameLayout.LayoutParams.MATCH_PARENT
-            ))
-            
-            // 基本设置
-            webView?.settings?.apply {
-                javaScriptEnabled = true
-                domStorageEnabled = true
+        // 创建 WebView
+        webView = WebView(this)
+        
+        setContentView(webView, FrameLayout.LayoutParams(
+            FrameLayout.LayoutParams.MATCH_PARENT,
+            FrameLayout.LayoutParams.MATCH_PARENT
+        ))
+        
+        // 基本设置
+        webView.settings.apply {
+            javaScriptEnabled = true
+            domStorageEnabled = true
+            cacheMode = android.webkit.WebSettings.LOAD_DEFAULT
+            allowFileAccess = true
+            setSupportZoom(true)
+        }
+        
+        // 设置 WebViewClient
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, request: android.webkit.WebResourceRequest?): Boolean {
+                view?.loadUrl(request?.url?.toString() ?: url)
+                return true
             }
-            
-            // 设置 WebViewClient
-            webView?.webViewClient = object : WebViewClient() {
-                override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
-                    view?.loadUrl(url ?: this@MainActivity.url)
-                    return true
+        }
+        
+        // 加载网页
+        webView.loadUrl(url)
+        
+        // 处理返回键
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (webView?.canGoBack() == true) {
+                    webView?.goBack()
+                } else {
+                    isEnabled = false
+                    onBackPressedDispatcher.onBackPressed()
                 }
             }
-            
-            // 加载网页
-            webView?.loadUrl(url)
-            
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-    }
-
-    override fun onBackPressed() {
-        if (webView?.canGoBack() == true) {
-            webView?.goBack()
-        } else {
-            super.onBackPressed()
-        }
+        })
     }
 
     override fun onDestroy() {
         webView?.destroy()
+        webView = null
         super.onDestroy()
     }
 }
