d7de5d9a9efed75635767b900e500be97f4abe34
ktkiss <114936588+ktkiss@users.noreply.github.com>
2026-04-29 10:28:30 +0800

    Simplify MainActivity for stability

--- app/src/main/java/com/xiaoke/bizhi/MainActivity.kt
+++ app/src/main/java/com/xiaoke/bizhi/MainActivity.kt
@@ -4,63 +4,68 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.webkit.WebSettings
 import android.widget.FrameLayout
-import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    private var webView: WebView? = null
+    private lateinit var webView: WebView
     private val url = "https://byd.xiaoke.name/"
 
-    @SuppressLint("SetJavaScriptEnabled")
+    @SuppressLint("SetJavaScriptEnabled", "AddJavascriptInterface")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        
+
+        // 使用 XML 布局更稳定
+        val frameLayout = FrameLayout(this)
+        setContentView(frameLayout)
+
         // 创建 WebView
-        webView = WebView(this)
-        
-        setContentView(webView, FrameLayout.LayoutParams(
+        webView = WebView(applicationContext)
+        frameLayout.addView(webView, FrameLayout.LayoutParams(
             FrameLayout.LayoutParams.MATCH_PARENT,
             FrameLayout.LayoutParams.MATCH_PARENT
         ))
-        
-        // 基本设置
-        webView.settings.apply {
-            javaScriptEnabled = true
-            domStorageEnabled = true
-            cacheMode = android.webkit.WebSettings.LOAD_DEFAULT
-            allowFileAccess = true
-            setSupportZoom(true)
-        }
-        
+
+        // 配置 WebView
+        val settings: WebSettings = webView.settings
+        settings.javaScriptEnabled = true
+        settings.domStorageEnabled = true
+        settings.loadWithOverviewMode = true
+        settings.useWideViewPort = true
+        settingsBuiltInZoomControls = true
+        settings.displayZoomControls = false
+        settings.cacheMode = WebSettings.LOAD_DEFAULT
+        settings.mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
+
         // 设置 WebViewClient
         webView.webViewClient = object : WebViewClient() {
-            override fun shouldOverrideUrlLoading(view: WebView?, request: android.webkit.WebResourceRequest?): Boolean {
-                view?.loadUrl(request?.url?.toString() ?: url)
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                view?.loadUrl(url ?: this@MainActivity.url)
                 return true
             }
         }
-        
+
         // 加载网页
         webView.loadUrl(url)
-        
-        // 处理返回键
-        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
-            override fun handleOnBackPressed() {
-                if (webView?.canGoBack() == true) {
-                    webView?.goBack()
-                } else {
-                    isEnabled = false
-                    onBackPressedDispatcher.onBackPressed()
-                }
-            }
-        })
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            @Suppress("DEPRECATION")
+            super.onBackPressed()
+        }
     }
 
     override fun onDestroy() {
-        webView?.destroy()
-        webView = null
+        if (::webView.isInitialized) {
+            webView.stopLoading()
+            webView.destroy()
+        }
         super.onDestroy()
     }
 }
